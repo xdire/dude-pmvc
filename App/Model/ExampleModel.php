b23df15f137a5489c52ef1a4e1b25abb25f39ab3
Anton Repin <dire.one@gmail.com>
2016-03-11 09:48:36 -0500

    Fixes 10

--- App/Model/ExampleModel.php
+++ App/Model/ExampleModel.php
@@ -1,13 +1,8 @@
 <?php
-/**
- * Created by Anton Repin.
- * Date: 3/10/16
- * Time: 9:50 PM
- */
 
 namespace App\Model;
 
-use Xdire\Dude\Xdire\Dude\Core\Model;
+use Xdire\Dude\Core\Model;
 
 class ExampleModel extends Model
 {
